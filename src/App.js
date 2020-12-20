5a477e220ab903351e206310409d8a759f0749ff
eaperches <edgar.a.perches@gmail.com>
2020-12-20 15:54:06 -0600

    fixed minor bug

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function ChatRoom() {
 function ChatMessage(props){
   const { text, uid, photoURL } = props.message;
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'sent';
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
     <div className={`message ${messageClass}`}>
